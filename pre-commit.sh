59c722c2c6cb02f52b9acdd50e4dca36e4a0aa62
Matthew Barton <wf5617@M0988-Mac.local>
2023-10-09 13:09:29 +0100

    Change python package name unittests workflow

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-
 black ./
 RESULT=$?
 if [[ ${RESULT} -ne 0 ]]; then
-        printf "Failed to format python scripts with black. Commit rejected"
-        exit ${RESULT}
+	printf "Failed to format python scripts with black. Commit rejected"
+	exit ${RESULT}
 fi
 
 shfmt -l -w ./
